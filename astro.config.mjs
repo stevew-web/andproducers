7a3ce1744d0da5322f0bd9a459cf7d354ea35e54
stevew-web <steve.w@productionstud.io>
2026-03-22 11:22:52 +0000

    Fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://andproducers.com',
-  integrations: [sitemap()],
-  markdown: {
-    shikiConfig: {
-      theme: 'github-light'
-    }
-  }
 });
